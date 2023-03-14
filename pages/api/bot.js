08b6c93568abb573773ea233e83a420b46887bac
gitfname <newamailnam@gmail.com>
2023-03-14 15:56:17 +0330

    first commit

---
+++ pages/api/bot.js
@@ -0,0 +1,10 @@
+import { Bot, webhookCallback } from "grammy";
+
+const bot = new Bot(process.env.BOT_TOKEN)
+
+
+bot.on("message", ctx => {
+    ctx.reply("hello world")
+})
+
+export default webhookCallback(bot, "http")
