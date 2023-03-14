ea8b5beefa50d0da6587ac676348350ff7dea0bf
gitfname <newamailnam@gmail.com>
2023-03-14 16:47:54 +0330

    13 commit

--- pages/api/bot.js
+++ pages/api/bot.js
@@ -3,12 +3,13 @@ import { Bot, webhookCallback } from "grammy";
 const bot = new Bot(process.env.BOT_TOKEN)
 
 
+bot.command("start", ctx => {
+    ctx.reply("settings here")
+})
+
 bot.on("message", ctx => {
     ctx.reply("hello world")
 })
 
-bot.command("start", ctx => {
-    ctx.reply("settings here")
-})
 
 export default webhookCallback(bot, "next-js")
